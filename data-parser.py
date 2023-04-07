5f8f8aa5f9016eafdbee8a211566dcafee48e118
Don Kuruppu <donkuruppu@Dons-MacBook-Pro.local>
2023-04-06 19:43:33 -0500

    fixing stuff

--- data-parser.py
+++ data-parser.py
@@ -31,7 +31,7 @@ def parse_iperf_data(file_path : str, start_point : int) -> pd.DataFrame:
     # Create a DataFrame and set column names
     df = pd.DataFrame(parsed_data, columns=['ID','Transfer_MBytes', 'Bandwidth_Mbits/sec'])
     
-    print(df)
+    #print(df)
     return df
     
 
@@ -54,14 +54,14 @@ def parse_ss_data(file_path : str, start_point : int) -> pd.DataFrame:
         b = b + 1
 
     df = pd.DataFrame(parsed_data, columns=['ID','Cwnd'])
-    print(df)
+    #print(df)
     return df
 
 
 
 def merge_df(df_one : pd.DataFrame, df_two : pd.DataFrame) -> pd.DataFrame:
     all_data_df = pd.merge(df_one, df_two, on='ID', how='left')
-    print(all_data_df)
+    #print(all_data_df)
     return all_data_df
 
 
@@ -71,10 +71,33 @@ if __name__ == '__main__':
     h1_h3_ss_df = parse_ss_data('host-one-ss-out.txt',0)
     h1_h3_iperf_df = parse_iperf_data('iperf_test_h1-h3_15s.txt',0)
 
-    h2_h4_ss_df = parse_ss_data('host-two-ss-out.txt',5)
-    h2_h4_iperf_df = parse_iperf_data('iperf_test_h2-h4_15s.txt',5)
+    h2_h4_ss_df = parse_ss_data('host-two-ss-out.txt',250)
+    h2_h4_iperf_df = parse_iperf_data('iperf_test_h2-h4_15s.txt',250)
 
     h1_h3_ss_iperef_df = merge_df(h1_h3_ss_df, h1_h3_iperf_df)
+    print(h1_h3_ss_iperef_df)
+    h2_h4_ss_iperf_df = merge_df(h2_h4_ss_df, h2_h4_iperf_df)
+    print(h2_h4_ss_iperf_df)
 
-    h1_h3_ss_iperef_df.plot(x='ID', y='Cwnd', kind='line')
-    plt.savefig('test.png')
+    #h1_h3_ss_iperef_df.plot(x='ID', y='Cwnd', kind='line')
+    #plt.savefig('test.png')
+
+    plt.figure(figsize=(8,6))
+    plt.plot(h1_h3_ss_iperef_df['ID'],h1_h3_ss_iperef_df['Cwnd'], label='host 1 to host 3', marker='o', linestyle='-')
+    plt.plot(h2_h4_ss_iperf_df['ID'],h2_h4_ss_iperf_df['Cwnd'], label='host 2 to host 4', marker='x', linestyle='-')
+    plt.xlabel('ID')
+    plt.ylabel('Cwnd')
+    plt.savefig('test2.png')
+
+    h1_h3_ss_iperef_df = h1_h3_ss_iperef_df.iloc[::20, :]
+    h2_h4_ss_iperf_df = h2_h4_ss_iperf_df.iloc[::20, :]
+
+    plt.figure(figsize=(8,6))
+    plt.plot(h1_h3_ss_iperef_df['ID'],h1_h3_ss_iperef_df['Bandwidth_Mbits/sec'], label='host 1 to host 3', marker='o', linestyle='-')
+    plt.plot(h2_h4_ss_iperf_df['ID'],h2_h4_ss_iperf_df['Bandwidth_Mbits/sec'], label='host 2 to host 4', marker='x', linestyle='-')
+    plt.xlabel('ID')
+    plt.ylabel('Bandwidth_Mbits/sec')
+    plt.savefig('test3.png')
+
+
+
--- dumbbell.py
+++ dumbbell.py
@@ -17,65 +17,77 @@ import signal
 
 class DumbbellTopo(Topo):
     def build(self, delay=2):
-        # The bandwidth (bw) is in Mbps, delay in milliseconds and queue size is in packets
-        br_params = dict(bw=984, delay='{0}ms'.format(delay), max_queue_size=82*delay,
-                         use_htb=True)  # backbone router interface tc params
-        ar_params = dict(bw=252, delay='0ms', max_queue_size=(21*delay*20)/100,
-                         use_htb=True)  # access router intf tc params
-        # TODO: remove queue size from hosts and try.
-        hi_params = dict(bw=960, delay='0ms', max_queue_size=80*delay, use_htb=True)  # host interface tc params
+            # The bandwidth (bw) is in Mbps, delay in milliseconds and queue size is in packets
+            # br_params = dict(bw=984, delay='{0}ms'.format(delay), max_queue_size=82*delay,
+                            # use_htb=True)  # backbone router interface tc params
+            # ar_params = dict(bw=252, delay='0ms', max_queue_size=(21*delay*20)/100,
+                            # use_htb=True)  # access router intf tc params
+            # TODO: remove queue size from hosts and try.
+            # hi_params = dict(bw=960, delay='0ms', max_queue_size=80*delay, use_htb=True)  # host interface tc params
+            br_params = dict(bw=984, delay='{0}ms'.format(delay), use_htb=True)
+            hi_params = dict(bw=960, use_htb=True)
 
-        # Create routers s1 to s4
-        s1 = self.addSwitch('s1') # Backbone router #1
-        s2 = self.addSwitch('s2') # Backbone router #2
-        s3 = self.addSwitch('s3') # Access router #1
-        s4 = self.addSwitch('s4') # Access router #2
+            # Create routers s1 to s4
+            s1 = self.addSwitch('s1')
+            s2 = self.addSwitch('s2')
+            s3 = self.addSwitch('s3')
+            s4 = self.addSwitch('s4')
 
-        # Link backbone routers (s1 & s2) together
-        self.addLink(s1, s2, cls=TCLink, **br_params) # Backbone Router #1 <-> Backbone Router #2
+            # Link backbone routers (s1 & s2) together
+            self.addLink(s1, s2, cls=TCLink, **br_params)
 
-        # Link access routers (s3 & s4) to the backbone routers
-        self.addLink(s1, s3, cls=TCLink, **ar_params) # Backbone Router #1 <-> Access Router #1
-        self.addLink(s2, s4, cls=TCLink, **ar_params) # Backbone Router #2 <-> Access Router #2
+            # Link access routers (s3 & s4) to the backbone routers
+            # self.addLink(s1, s3, cls=TCLink, **ar_params)
+            # self.addLink(s2, s4, cls=TCLink, **ar_params)
+            self.addLink(s1, s3, cls=TCLink)
+            self.addLink(s2, s4, cls=TCLink)
 
-        # Create the hosts h1 to h4, and link them to access router 1
-        h1 = self.addHost('h1') # Source #1
-        h2 = self.addHost('h2') # Source #2
-        h3 = self.addHost('h3') # Receiver #1
-        h4 = self.addHost('h4') # Receiver #2
+            # Create the hosts h1 to h4, and link them to access router 1
+            h1 = self.addHost('h1')
+            h2 = self.addHost('h2')
+            h3 = self.addHost('h3')
+            h4 = self.addHost('h4')
 
-        # Link the source hosts (h1 & h3) to access router 1 (s3)
-        self.addLink(s3, h1, cls=TCLink, **hi_params) # Source #1 <-> Access router #1
-        self.addLink(s3, h3, cls=TCLink, **hi_params) # Source #2 <-> Access router #1
+            # Link the source hosts (h1 & h3) to access router 1 (s3)
+            self.addLink(s3, h1, cls=TCLink, **hi_params)
+            self.addLink(s3, h3, cls=TCLink, **hi_params)
 
-        # Link the receiver hosts (h2 & h4) to access router 2 (s4)
-        self.addLink(s4, h2, cls=TCLink, **hi_params) # Receiver #1 <-> Access Router #2
-        self.addLink(s4, h4, cls=TCLink, **hi_params) # Receiver #2 <-> Access Router #2
+            # Link the receiver hosts (h2 & h4) to access router 2 (s4)
+            self.addLink(s4, h2, cls=TCLink, **hi_params)
+            self.addLink(s4, h4, cls=TCLink, **hi_params)
 
+            # Configure the vertical links connecting the access routers to the backbone routers
+            bufferSize = ((20 * 21 * 1500) / (100 * 1000)) * delay
+            quietRun(f'tc qdisc add dev s1-eth2 root tbf rate 252mbit burst 100kb limit {bufferSize}kb')
+            quietRun(f'tc qdisc add dev s3-eth1 root tbf rate 252mbit burst 100kb limit {bufferSize}kb')
+            quietRun(f'tc qdisc add dev s2-eth2 root tbf rate 252mbit burst 100kb limit {bufferSize}kb')
+            quietRun(f'tc qdisc add dev s4-eth1 root tbf rate 252mbit burst 100kb limit {bufferSize}kb')
 
-def iperf3_command_builder(ip_address, port, interval, mtu, length_time_seconds, file_path, file_output_name) -> str:
-    ''' Example: iperf3 --forceflush -c {0} -p 1111 -i 0.5 -M 1460 -N -t 20 > iperf_test_h1-h3_15s.txt
-        All arguments must be passed in as strings'''
-    command = ''
-    command += 'iperf3' # iperf3 command
-    command += ' '
-    command += '--forceflush' # ensures that all data iperf collects is pushed to the txt file
-    command += ' '
-    command += '-c ' + ip_address # sets server ip address
-    command += ' '
-    command += '-p ' + port # sets server port
-    command += ' '
-    command += '-i ' + interval # sets interval to output data to txt file
-    command += ' '
-    command += '-M ' + mtu # sets the minimum transmission unit
-    command += ' '
-    command += '-N' # no delay
-    command += ' '
-    command += '-t ' + length_time_seconds # how long to send data for, in seconds
-    command += ' > '
-    command += file_path + file_output_name + '.txt' # file path and file name of data collected
-    return command
+# def iperf3_command_builder(ip_address, port, interval, mtu, length_time_seconds, file_path, file_output_name) -> str:
+#     ''' Example: iperf3 --forceflush -c {0} -p 1111 -i 0.5 -M 1460 -N -t 20 > iperf_test_h1-h3_15s.txt
+#         All arguments must be passed in as strings'''
+#     command = ''
+#     command += 'iperf3' # iperf3 command
+#     command += ' '
+#     command += '--forceflush' # ensures that all data iperf collects is pushed to the txt file
+#     command += ' '
+#     command += '-c ' + ip_address # sets server ip address
+#     command += ' '
+#     command += '-p ' + port # sets server port
+#     command += ' '
+#     command += '-i ' + interval # sets interval to output data to txt file
+#     command += ' '
+#     command += '-M ' + mtu # sets the minimum transmission unit
+#     command += ' '
+#     command += '-N' # no delay
+#     command += ' '
+#     command += '-t ' + length_time_seconds # how long to send data for, in seconds
+#     command += ' > '
+#     command += file_path + file_output_name + '.txt' # file path and file name of data collected
+#     return command
 
+def change_tcp_congestion_algorithm(algo):
+    quietRun(f"sysctl -w net.ipv4.tcp_congestion_control={algo}")
 
 def dumbbell_test():
     duration_one_ms = 0
@@ -98,25 +110,23 @@ def dumbbell_test():
     h4_ip = h4.IP() # Receiver #2
 
     sub_processes = dict()
-    sub_processes[h3] = h3.popen('iperf -s -p 1111') # Start server on Receiver #1
-    sub_processes[h4] = h4.popen('iperf -s -p 2222') # Start server on Receiver #2
+    sub_processes[h3] = h3.popen('nohup iperf -s -p 1111') # Start server on Receiver #1
+    sub_processes[h4] = h4.popen('nohup iperf -s -p 2222') # Start server on Receiver #2
 
     # Start client on Source #1
-    command_one = iperf3_command_builder(h3_ip, '1111', '0.5', '1460', str(duration_one_ms),'/school/','output')
-    print(command_one)
-    sub_processes[h1] = h1.popen('iperf --forceflush -c {0} -p 1111 -i 1 -M 1460 -N -t 20 > iperf_test_h1-h3_15s.txt'.format(h3_ip), shell=True)
+
+    sub_processes[h1] = h1.popen('nohup iperf --forceflush -c {0} -p 1111 -i 1 -f m -N -t 2000 > iperf_test_h1-h3_15s.txt'.format(h3_ip), shell=True)
     cwd_ss_one = h1.popen('watch -n 1 \'ss --tcp -i dst {0} >> host-one-ss-out.txt\''.format(h3_ip), shell=True)
-    print('Source #1 Client Started')
+
+    time.sleep(250)
+
     # Start client on Source #2
-    time.sleep(5)
     print('Source #2 Client Started')
-    sub_processes[h2] = h2.popen('iperf --forceflush -c {0} -p 2222 -i 1 -M 1460 -N -t 15 > iperf_test_h2-h4_15s.txt'.format(h4_ip), shell=True)
+    sub_processes[h2] = h2.popen('nohup iperf --forceflush -c {0} -p 2222 -i 1 -f m -N -t 1750 > iperf_test_h2-h4_15s.txt'.format(h4_ip), shell=True)
     cwd_ss_two = h2.popen('watch -n 1 \'ss --tcp -i dst {0} >> host-two-ss-out.txt\''.format(h4_ip), shell=True)
 
     sub_processes[h1].wait() # Wait for Source #1 to stop sending
     sub_processes[h2].wait() # Wait for Source #2 to stop sending
-
-
 
     sub_processes[h3].terminate() # Stop Receiver #1 server
     sub_processes[h4].terminate() # Stop Receiver #2 server
