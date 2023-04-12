b45e3fa9256873f4de6e8a93e061aac4d070c586
Sammy <samalaniz25@gmail.com>
2023-04-12 04:01:09 +0000

    folder dirs look good, need more testing

--- session.py
+++ session.py
@@ -5,11 +5,30 @@ DELAYS = [21, 81, 162]
 
 ALGOS = ['reno', 'cubic', 'vegas', 'westwood']
 
+starting_working_dir = os.getcwd()
+
 try:
 
     for algo in ALGOS:
+
+        # make algo folder to hold algo specific data
+        if not os.path.exists(algo):
+            os.mkdir(algo)
+
+        # change directory to the algo directory
+        os.chdir( starting_working_dir + '/' + algo )
+
         for delay in DELAYS:
+            delay_dir = str(delay) + '_miliseconds'
+
+            # make delay folder to hold specific algo/delay data
+            if not os.path.exists(delay_dir):
+                os.mkdir(delay_dir)
+            
+            os.chdir( starting_working_dir + '/' + algo + '/' + delay_dir )
+
             clean_result = subprocess.run(['sudo','mn','-c'], stdout=subprocess.PIPE)
             dumbbell_test(delay, algo)
+
 except SystemExit:
     print('\nprogram ended early')
